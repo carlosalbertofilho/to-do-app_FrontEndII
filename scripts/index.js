7802b6289b899742fdca94a94ce8bb68b19e9e97
Denis <arboness@gmail.com>
2022-03-28 20:08:46 -0300

    referencias

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+let inputEmailReference = document.querySelector('#inputEmail')
+let inputPasswordReference = document.querySelector('#inputPassword')
+let submitReference = document.querySelector('#submit')
+
+submitReference.addEventListener('click',function(event){
+
+inputEmailReference = inputEmailReference.value
+
+
+
+})

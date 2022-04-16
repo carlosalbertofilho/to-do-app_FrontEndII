adc51d79541e01cbfb46e519d8c7b7c82250e64e
Denis <arboness@gmail.com>
2022-04-16 13:47:31 -0300

    utilizaçao do sweetalert2 na confirmacao do delete

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -244,15 +244,23 @@ function getTasks(){
 
 //Confirm Delete
 function confirmDelete(deleteId){
-
-    confirm('Do you really want delete this task?')
-    if (confirm){
-        deleteTask(deleteId)
-    }else{
-
+ 
+    Swal.fire({
+        title: 'Do you really want delete this task?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            deleteTask(deleteId)
+          )
+        }
+      })
     }
-
-}
 
 
 //Delete Task
@@ -282,5 +290,3 @@ function deleteTask(id){
             )          
             
         }
-
-        
